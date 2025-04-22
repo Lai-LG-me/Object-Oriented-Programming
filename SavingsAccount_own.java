80edd045dbb3edaa201b8712fb6a8f67eec1d190
Lai-LG-me <lgginnie0510@gmail.com>
2025-04-22 23:26:08 +0800

    Create SavingsAccount_own.java

---
+++ SavingsAccount_own.java
@@ -0,0 +1,28 @@
+// Still got ERROR
+
+// For the SavingsAccount class, include statements 
+// to test the methods for 
+// calculating the interest amount and adding the interest to the balance.
+
+public class SavingsAccount_own extends Account_own {
+		private double interest_rate = 0.05;
+			
+		public SavingsAccount_own(String accountNum, double balance){		
+			super(accountNum, balance);
+		}
+		
+		//----------Getter----------------
+		public double getInterestRate(){
+			return interest_rate;
+		}
+		public void setInterestRate(double interest_rate){
+			this.interest_rate = interest_rate;
+		}
+		
+		//----------Calculate interest amount----------------
+		public double CalculateInterestAmount(){
+			return (getBalance() * interest_rate / 12);		// the interest rate is per year , /12 for per month
+		}
+
+
+}
